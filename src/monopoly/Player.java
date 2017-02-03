c5f75a0db0dc58dc7ecca95f673dcb88ce2153a0
Conal O'Neill <conal.oneill95@gmail.com>
2017-02-03 10:00:27 +0000

    player colour

--- src/monopoly/Player.java
+++ src/monopoly/Player.java
@@ -1,11 +1,17 @@
 package monopoly;
 
+import java.awt.Color;
+
 public class Player {
 	
 	Player(int startingTile, int playerNum){
 		currentTile = startingTile;
 		playerNumber = playerNum;
 	}
+	
+	private Color colour;
+	
+	
 	
 	public int playerNumber;
 	private int currentTile;
